70c3a975ab554a0d5b79d08a8ebd4609021dbad5
mabuqama <mohammadabu-qamar@outlook.com>
2026-05-16 18:20:35 +0800

    The new start of main HDL code

--- src/top_module.vhd
+++ src/top_module.vhd
@@ -5,11 +5,15 @@ use IEEE.NUMERIC_STD.ALL;
 entity top_module is
 	port (
 		i_Clock     : in  STD_LOGIC;
+		i_Reset    : in  STD_LOGIC;
 		i_Button    : in  STD_LOGIC;
 		o_UART_TX   : out STD_LOGIC;
 		i_UART_RX   : in  STD_LOGIC;
+		i_Pressure_Sensor   : in  STD_LOGIC;
 		o_Motor_A_A  : out STD_LOGIC; 
 		o_Motor_A_B  : out STD_LOGIC;
+		o_Motor_B_A  : out STD_LOGIC; 
+		o_Motor_B_B  : out STD_LOGIC;
 		i_Switch    : in  STD_LOGIC_VECTOR (9 downto 0);
 		o_LEDR      : out STD_LOGIC_VECTOR (9 downto 0)
 	);
@@ -21,26 +25,88 @@ architecture Behavioral of top_module is
 
 	constant c_DC_Motor_PWM_FREQUENCY : INTEGER := 200_000;
 
-begin
-	Motor_Driver_init : entity work.L9110_2_CHANNEL_MOTOR_DRIVER
-		generic map(
-			g_CLOCK_FREQUENCY => c_CLOCK_FREQUENCY,
-			g_A_PWM_FREQUENCY => c_DC_Motor_PWM_FREQUENCY,
-			g_B_PWM_FREQUENCY => c_DC_Motor_PWM_FREQUENCY
-		)
-		port map(
-			i_A_Speed => i_Switch(7 downto 0),
-			i_A_Direction => not i_Button,
-			i_B_Speed => (others => '0'),
-			i_B_Direction => '0',
-			i_Clock => i_Clock,
-			o_A_i_A => o_Motor_A_A,
-			o_A_i_B => o_Motor_A_B,
-			o_B_i_A => open,
-			o_B_i_B => open
-	);
-	o_LEDR(7 downto 0) <= i_Switch(7 downto 0);
-	o_LEDR(8) <= o_Motor_A_A;
-	o_LEDR(9) <= o_Motor_A_B;
+	constant c_TWO_SECONDS : INTEGER := 100_000_000;
+
+	type t_Section_01 is (
+			s_IDLE, s_BALL_DELAY, s_ACTUATE
+		);
+
+	signal r_Section_01 : t_Section_01 := s_IDLE;
+
+	signal r_Counter_Section_01 : INTEGER range 0 to c_TWO_SECONDS := 0;
+
+	signal w_Actuator : STD_LOGIC := '0';
+	signal w_Reset : STD_LOGIC;
+
+	begin
+		----------------------------------------------------------------------------------
+		-- Static assignment
+		----------------------------------------------------------------------------------
+		w_Reset <= not i_Reset;
+		o_LEDR(8) <= o_Motor_A_A;
+		o_LEDR(9) <= o_Motor_A_B;
+		o_LEDR(0) <= not i_Pressure_Sensor;
+
+		----------------------------------------------------------------------------------
+		-- Component decleration
+		----------------------------------------------------------------------------------
+		Motor_Driver_init : entity work.L9110_2_CHANNEL_MOTOR_DRIVER
+			generic map(
+				g_CLOCK_FREQUENCY => c_CLOCK_FREQUENCY,
+				g_A_PWM_FREQUENCY => c_DC_Motor_PWM_FREQUENCY,
+				g_B_PWM_FREQUENCY => c_DC_Motor_PWM_FREQUENCY
+			)
+			port map(
+				i_A_Speed => (others => w_Actuator),
+				i_A_Direction => '0',
+				i_B_Speed => (others => '0'),
+				i_B_Direction => '0',
+				i_Clock => i_Clock,
+				o_A_i_A => o_Motor_A_A,
+				o_A_i_B => o_Motor_A_B,
+				o_B_i_A => o_Motor_B_A,
+				o_B_i_B => o_Motor_B_B
+		);
+
+		p_SECTION_01 : process(i_Clock, i_Reset) begin
+			if w_Reset = '1' then
+				r_Section_01 <= s_IDLE;
+
+			elsif rising_edge(i_Clock)then
+				case r_Section_01 is
+					when s_IDLE =>
+						r_Section_01 <= s_IDLE;
+						r_Counter_Section_01 <= 0;
+						w_Actuator <= '0';
+						if i_Pressure_Sensor = '1' then
+							r_Section_01 <= s_BALL_DELAY;
+						end if;
+
+					when s_BALL_DELAY =>
+						r_Section_01 <= s_BALL_DELAY;
+						if r_Counter_Section_01 < c_TWO_SECONDS - 1 then
+							r_Section_01 <= s_BALL_DELAY;
+							r_Counter_Section_01 <= r_Counter_Section_01 + 1;
+						else
+							r_Counter_Section_01 <= 0;
+							w_Actuator <= '1';
+							r_Section_01 <= s_ACTUATE;
+						end if;
+
+					when s_ACTUATE =>
+						r_Section_01 <= s_ACTUATE;
+						w_Actuator <= '1';
+						if r_Counter_Section_01 < c_TWO_SECONDS - 1 then
+							r_Section_01 <= s_ACTUATE;
+							r_Counter_Section_01 <= r_Counter_Section_01 + 1;
+						else
+							r_Counter_Section_01 <= 0;
+							w_Actuator <= '0';
+							r_Section_01 <= s_IDLE;
+						end if;
+				end case;
+			end if;
+
+		end process p_SECTION_01;
 
 end Behavioral;
